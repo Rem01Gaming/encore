40192fb605d1825f4413e1c59c278fa4e580e5a2
Rem01Gaming <Rem01_Gaming@proton.me>
2024-09-16 22:08:13 +0700

    webui: Fix parcel build error
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- src/webui/src/webui_utils.js
+++ src/webui/src/webui_utils.js
@@ -84,28 +84,32 @@ async function populateCPUGovernors() {
   if (govErrno === 0) {
     const governors = govStdout.trim().split(/\s+/);
     const selectElement1 = document.getElementById('cpuGovernor');
-    const selectElement2 = document.getElementById('cpuGovernor');
+    const selectElement2 = document.getElementById('cpuGovernorPerf');
 
     selectElement1.innerHTML = '';
     selectElement2.innerHTML = '';
 
     governors.forEach(gov => {
-      const option = document.createElement('option');
-      option.value = gov;
-      option.textContent = gov;
-      selectElement.appendChild(option);
-      selectElement1.appendChild(option);
+      const option1 = document.createElement('option');
+      option1.value = gov;
+      option1.textContent = gov;
+      selectElement1.appendChild(option1);
+
+      const option2 = document.createElement('option');
+      option2.value = gov;
+      option2.textContent = gov;
+      selectElement2.appendChild(option2);
     });
 
-    let { errno, stdout } = await exec('encore-utils get_default_cpugov');
-    if (errno === 0) {
-      const defaultGovernor = stdout.trim();
+    const { errno: defaultErrno, stdout: defaultStdout } = await exec('encore-utils get_default_cpugov');
+    if (defaultErrno === 0) {
+      const defaultGovernor = defaultStdout.trim();
       selectElement1.value = defaultGovernor;
     }
-    
-    let { errno, stdout } = await exec('encore-utils get_perf_cpugov');
-    if (errno === 0) {
-      const defaultPerfGovernor = stdout.trim();
+
+    const { errno: perfErrno, stdout: perfStdout } = await exec('encore-utils get_perf_cpugov');
+    if (perfErrno === 0) {
+      const defaultPerfGovernor = perfStdout.trim();
       selectElement2.value = defaultPerfGovernor;
     }
   }
