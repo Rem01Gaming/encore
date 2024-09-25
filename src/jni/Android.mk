09bd66fbb724a4b8e8d29aa19d65e3a063442d24
Rem01Gaming <Rem01_Gaming@proton.me>
2024-09-25 13:19:59 +0700

    jni: Add vmtouch to build * Prepare for memory preload feature
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- src/jni/Android.mk
+++ src/jni/Android.mk
@@ -1,9 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE    := encore-service
+LOCAL_MODULE := encore-service
 LOCAL_SRC_FILES := encore-service.c
 LOCAL_LDFLAGS := -static
+LOCAL_CFLAGS := -Wall -O2
+include $(BUILD_EXECUTABLE)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := vmtouch
+LOCAL_SRC_FILES := vmtouch.c
+LOCAL_LDFLAGS := -static
+LOCAL_CFLAGS := -Wall -O2 -g -std=c99
 include $(BUILD_EXECUTABLE)
