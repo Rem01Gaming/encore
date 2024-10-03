63d5453fa0790fafa96b8a0455eec05ad8d83308
Rem01Gaming <Rem01_Gaming@proton.me>
2024-10-03 19:30:37 +0700

    Gamelist: Add more games
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- gamelist_compile.sh
+++ gamelist_compile.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-echo "com.example.gamelist1|com.example.gamelist2$(awk '!/^[[:space:]]*$/ && !/^#/ && !(/[[:alnum:]]+[[:space:]]+[[:alnum:]]+[[:space:]]+[[:alnum:]]+/) {sub("-e ", ""); printf "|%s", $0}' "./gamelist.txt")" >module/gamelist.txt
+echo "com.example.game$(awk '!/^[[:space:]]*$/ && !/^#/ && !(/[[:alnum:]]+[[:space:]]+[[:alnum:]]+[[:space:]]+[[:alnum:]]+/) {sub("-e ", ""); printf "|%s", $0}' "./gamelist.txt")" >module/gamelist.txt
