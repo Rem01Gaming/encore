0845ca40878ded508ad61c8e2c5b56f7705681cd
Rem01Gaming <Rem01_Gaming@proton.me>
2024-10-06 21:11:14 +0700

    module: Run Soc info while installation
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- module/customize.sh
+++ module/customize.sh
@@ -67,6 +67,35 @@ fi
 ui_print "- Permission setup"
 set_perm_recursive "$MODPATH/system/bin" 0 0 0755 0755
 
+# Determine Chipset
+chipset=$(grep "Hardware" /proc/cpuinfo | uniq | cut -d ':' -f 2 | sed 's/^[ \t]*//')
+
+if [ -z "$chipset" ]; then
+	chipset="$(getprop ro.board.platform) $(getprop ro.hardware)"
+fi
+
+case "$chipset" in
+*mt* | *MT*) soc=1 && ui_print "- Implementing tailored tweaks for Mediatek" ;;
+*sm* | *qcom* | *SM* | *QCOM* | *Qualcomm*) soc=2 && ui_print "- Implementing tailored tweaks for Snapdragon" ;;
+*exynos*) soc=3 && ui_print "- Implementing tailored tweaks for Exynos" ;;
+*Unisoc* | *unisoc*) soc=4 && ui_print "- Implementing tailored tweak for Unisoc" ;;
+*gs*) soc=5 && ui_print "- Implementing tailored tweaking for Google Tensor" ;;
+*)
+	if [ -f /sys/devices/soc0/machine ] && [ ! -d /sys/kernel/gpu ]; then
+		soc=2
+		ui_print "- Implementing tailored tweaks for Snapdragon"
+	else
+		soc=0
+	fi
+	;;
+esac
+
+if [ $soc -eq 0 ]; then
+	ui_print "- Unknown SoC manufacturer, skipping some tweaks implementation"
+fi
+
+echo $soc >/data/encore/soc_recognition
+
 ui_print ""
 ui_print "- A long, long time ago... Cosmos told Encore about a fun spot!"
 ui_print "- Come on! Let's go check it out ~"
--- module/service.sh
+++ module/service.sh
@@ -3,29 +3,8 @@ while [ -z "$(getprop sys.boot_completed)" ]; do
 	sleep 40
 done
 
-# CPU info
-chipset=$(grep "Hardware" /proc/cpuinfo | uniq | cut -d ':' -f 2 | sed 's/^[ \t]*//')
-
-if [ -z "$chipset" ]; then
-	chipset=$(getprop "ro.board.platform")
-fi
-
-case $chipset in
-*mt* | *MT*) soc=1 ;;
-*sm* | *qcom* | *SM* | *QCOM* | *Qualcomm*) soc=2 ;;
-*exynos*) soc=3 ;;
-*Unisoc* | *unisoc*) soc=4 ;;
-*gs*) soc=5 ;;
-*) soc=0 ;;
-esac
-
-if [ $soc -eq 0 ] && [ -f /sys/devices/soc0/machine ]; then
-	soc=2
-fi
-
 chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor >/data/encore/default_cpu_gov
-echo $soc >/data/encore/soc_recognition
 
 rm -f /data/encore/last_fault
 touch /data/encore/last_fault
