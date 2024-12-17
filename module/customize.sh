17900ac5ed80d80553c8d40bb94f9281a4eea234
Rem01Gaming <Rem01_Gaming@proton.me>
2024-12-17 18:37:33 +0700

    module/customize: Remove game preload config
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- module/customize.sh
+++ module/customize.sh
@@ -50,7 +50,6 @@ unzip -o "$ZIPFILE" "webroot/*" -d "$MODPATH" >&2
 ui_print "- Encore Tweaks configuration setup"
 [ ! -d /data/encore ] && mkdir /data/encore
 [ ! -f /data/encore/kill_logd ] && echo 0 >/data/encore/kill_logd
-[ ! -f /data/encore/game_preload ] && echo 0 >/data/encore/game_preload
 extract "$ZIPFILE" 'encore_logo.png' "/data/local/tmp"
 extract "$ZIPFILE" 'gamelist.txt' "/data/encore"
 
