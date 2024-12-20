8304b4ee8639691d072bc64658ea2e76357c7903
Rem01Gaming <Rem01_Gaming@proton.me>
2024-12-20 15:46:08 +0700

    jni: Optimize things
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- src/jni/Android.mk
+++ src/jni/Android.mk
@@ -4,5 +4,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := encored
 LOCAL_SRC_FILES := encored.c
 LOCAL_LDFLAGS := -static
-LOCAL_CFLAGS := -Wall -Wextra -Werror -pedantic-errors -Wpedantic -O2
+LOCAL_CFLAGS := -DNDEBUG -Wall -Wextra -Werror -pedantic-errors -Wpedantic -O2
 include $(BUILD_EXECUTABLE)
--- src/jni/Application.mk
+++ src/jni/Application.mk
@@ -1,2 +1,3 @@
 APP_ABI := all
+APP_OPTIM := release
 APP_PLATFORM := android-29
