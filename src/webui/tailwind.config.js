49f247a952d73d99966709b71942f9e23f685966
Rem01Gaming <Rem01_Gaming@proton.me>
2024-12-26 16:16:08 +0700

    webui: Update packages and integrate tailwind with parcel
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- src/webui/tailwind.config.js
+++ src/webui/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
