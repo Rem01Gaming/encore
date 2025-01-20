12eb1d4b815d5c70bba1c6bbc01ef2f14d4a6eeb
Rem01Gaming <Rem01_Gaming@proton.me>
2025-01-20 22:24:03 +0700

    webui: Implement material you color via MMRL CSS
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- src/webui/tailwind.config.js
+++ src/webui/tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "blue-background": "#0F141B",
-        "blue-secondary": "#394253",
-        "blue-container": "#161D27",
-        "blue-primary": "#99BBFF",
+        "primary": "var(--primary, #99BBFF)",
+        "on-primary": "var(--onPrimary, #000000)",
+        "secondary": "var(--secondary, #394253)",
+        "on-secondary": "var(--onSecondary, #FFFFFF)",
+        "surface": "var(--surface, #0F141B)",
+        "on-surface": "var(--onSurface, #FFFFFF)",
+        "on-surface-variant": "var(--onSurfaceVariant, #9ca3af)",
+        "surface-container": "var(--surfaceContainer, #161D27)",
       },
     },
   },
