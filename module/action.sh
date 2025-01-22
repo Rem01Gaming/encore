aaf0dc201a0d2a76e75108d72681c4951cc6a975
Rem01Gaming <Rem01_Gaming@proton.me>
2025-01-22 10:18:15 +0700

    scripts/encore_profiler: Disable kernel panic
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- module/action.sh
+++ module/action.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ -z "$MMRL" ] && [ ! -z "$MAGISKTMP" ]; then
-	pm path io.github.a13e300.ksuwebui > /dev/null 2>&1 && {
+	pm path io.github.a13e300.ksuwebui >/dev/null 2>&1 && {
 		echo "- Launching WebUI in KSUWebUIStandalone..."
 		am start -n "io.github.a13e300.ksuwebui/.WebUIActivity" -e id "encore"
 		exit 0
 	}
-	pm path com.dergoogler.mmrl > /dev/null 2>&1 && {
+	pm path com.dergoogler.mmrl >/dev/null 2>&1 && {
 		echo "- Launching WebUI in MMRL WebUI..."
 		am start -n "com.dergoogler.mmrl/.ui.activity.webui.WebUIActivity" -e MOD_ID "encore"
 		exit 0
