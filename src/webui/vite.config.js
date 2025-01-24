88b36f37c4d02d209e169d0323a3483b0359ccf7
Rem01Gaming <Rem01_Gaming@proton.me>
2025-01-25 05:10:11 +0700

    webui: Minify HTML
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- src/webui/vite.config.js
+++ src/webui/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite"
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
   root: './src',
+  plugins: [
+    ViteMinifyPlugin({}),
+  ],
   css: {
     postcss: {
       plugins: [tailwind, autoprefixer],
