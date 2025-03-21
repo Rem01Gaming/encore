e83a247d38e0b73d5302fabf0ce367ba4c25969f
Rem01Gaming <Rem01_Gaming@proton.me>
2025-03-21 13:43:18 +0700

    module/uninstall: Fix shellcheck warning
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -6,7 +6,7 @@ need_gone="encored encore_profiler encore_utility"
 manager_paths="/data/adb/ap/bin /data/adb/ksu/bin"
 
 for dir in $manager_paths; do
-	[ -d $dir ] && {
+	[ -d "$dir" ] && {
 		for bin in $need_gone; do
 			rm "$dir/$bin"
 		done
