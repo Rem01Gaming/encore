c945b1749ec617a73a4dabd54c166a38923257fe
Rem01Gaming <Rem01_Gaming@proton.me>
2025-04-07 18:40:31 +0700

    module/verify: Simplfy the extract function
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- module/verify.sh
+++ module/verify.sh
@@ -9,35 +9,23 @@ abort_verify() {
 	abort "*********************************************************"
 }
 
-# extract <zip> <file> <target dir> <junk paths>
+# extract <zip> <file> <target dir>
 extract() {
 	zip=$1
 	file=$2
 	dir=$3
-	junk_paths=$4
-	[ -z "$junk_paths" ] && junk_paths=false
-	opts="-o"
-	[ "$junk_paths" = true ] && opts="-oj"
 
-	if [ "$junk_paths" = true ]; then
-		file_path="$dir/$(basename "$file")"
-		hash_path="$TMPDIR_FOR_VERIFY/$(basename "$file").sha256"
-	else
-		file_path="$dir/$file"
-		hash_path="$TMPDIR_FOR_VERIFY/$file.sha256"
-	fi
+	file_path="$dir/$file"
+	hash_path="$TMPDIR_FOR_VERIFY/$file.sha256"
 
-	unzip $opts "$zip" "$file" -d "$dir" >&2
+	unzip -o "$zip" "$file" -d "$dir" >&2
 	[ -f "$file_path" ] || abort_verify "$file does not exists"
 
-	unzip $opts "$zip" "$file.sha256" -d "$TMPDIR_FOR_VERIFY" >&2
+	unzip -o "$zip" "$file.sha256" -d "$TMPDIR_FOR_VERIFY" >&2
 	[ -f "$hash_path" ] || abort_verify "Missing checksum for $file"
 
 	(echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -) || abort_verify "Checksum mismatch for $file"
 	ui_print "- Verified $file" >&1
-
-	unset file_path
-	unset hash_path
 }
 
 file="META-INF/com/google/android/update-binary"
