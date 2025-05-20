4b8b8ae68dd135b49c41d4f413be8a4a5d3e6166
Rem01Gaming <Rem01_Gaming@proton.me>
2025-05-20 07:44:54 +0700

    webui: Import MMRL CSS manually
    Seems that mmrl.injectStyleSheets() from MMRL npm package is broken, let's import
    it manually.
    
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- webui/src/scripts/mmrl.js
+++ webui/src/scripts/mmrl.js
@@ -1,9 +1,6 @@
 import { MMRLInterfaceFactory } from "mmrl";
 const mmrl = MMRLInterfaceFactory("encore");
 
-// inject MMRL CSS
-mmrl.injectStyleSheets();
-
 // handle no-js-api permission
 if (!mmrl.hasAccessToAdvancedKernelSuAPI) {
     if (!mmrl.requestAdvancedKernelSUAPI()) {
