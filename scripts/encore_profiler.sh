af70840fd8953e46c43134a5d8d65074a9262efd
Rem01Gaming <Rem01_Gaming@proton.me>
2025-06-04 08:07:41 +0700

    scripts/encore_profiler: Don't let perfmgr mess with CPU governor
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- scripts/encore_profiler.sh
+++ scripts/encore_profiler.sh
@@ -64,6 +64,7 @@ change_cpu_gov() {
 	chmod 644 /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 	echo "$1" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor >/dev/null
 	chmod 444 /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+	chmod 444 /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
 }
 
 set_dnd() {
