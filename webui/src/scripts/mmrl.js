224c1cfd1854751ac86aa1744496dd2b0858366d
Rem01Gaming <Rem01_Gaming@proton.me>
2025-06-14 07:49:36 +0000

    webui: Remove MMRL permission check
    MMRL already drop this a long time ago, even their WebUI system just moved
    into WebUI X, we don't need this anymore.
    
    This commit remove MMRL permission check alongside with translation and the
    MMRL package.
    
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- webui/src/scripts/mmrl.js
+++
@@ -1,9 +0,0 @@
-import { MMRLInterfaceFactory } from "mmrl";
-const mmrl = MMRLInterfaceFactory("encore");
-
-// handle no-js-api permission
-if (!mmrl.hasAccessToAdvancedKernelSuAPI) {
-    if (!mmrl.requestAdvancedKernelSUAPI()) {
-        mmrl_denied.showModal();
-    }
-}
