1f0e13f6fe4042d3844b28d177da8e226d0537f6
Rem01Gaming <Rem01_Gaming@proton.me>
2025-09-14 08:09:59 +0000

    website: Remove theme switch transision effect
    While this effect is nice, it have some bugs including white flash on theme transition from light to
    dark and the toggle switch won't animate like used to.
    
    Hence, remove it.
    
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- website/docs/.vitepress/theme/index.js
+++ website/docs/.vitepress/theme/index.js
@@ -1,10 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
-import { h } from 'vue'
 import './custom.css'
-import MyLayout from "./components/Layout.vue";
 
-
-export default {
-  extends: DefaultTheme,
-  Layout: MyLayout,
-}
+export default DefaultTheme
