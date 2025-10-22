d1e8796af848fb93b2470499fa284d10294b4c79
Rem01Gaming <Rem01_Gaming@proton.me>
2025-10-22 12:59:45 +0700

    service: Cleanup pragma guard and license header
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- jni/include/Encore.hpp
+++ jni/include/Encore.hpp
@@ -1,21 +1,20 @@
 /*
-* Copyright (C) 2024-2025 Rem01Gaming
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+ * Copyright (C) 2024-2025 Rem01Gaming
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#ifndef ENCORE_H
-#define ENCORE_H
+#pragma once
 
 #include <string>
 
@@ -42,5 +41,3 @@ struct EncoreGameList {
     bool lite_mode;
     bool enable_dnd;
 };
-
-#endif // ENCORE_H
--- jni/include/Write2File.hpp
+++ jni/include/Write2File.hpp
@@ -1,18 +1,20 @@
 /*
-* Copyright (C) 2024-2025 Rem01Gaming
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+ * Copyright (C) 2024-2025 Rem01Gaming
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+#pragma once
 
 #include <fstream>
 #include <sstream>
@@ -27,16 +29,16 @@
 * @param args The arguments to format into the string.
 * @return true if the write operation was successful, false otherwise.
 */
-template<typename... Args>
-bool write2file(const std::string& filename, Args&&... args) {
-   std::ostringstream oss;
-   (oss << ... << std::forward<Args>(args));
+template <typename... Args>
+bool write2file(const std::string &filename, Args &&...args) {
+    std::ostringstream oss;
+    (oss << ... << std::forward<Args>(args));
 
-   std::ofstream file(filename, std::ios::trunc);
-   if (!file.is_open()) {
-       return false;
-   }
+    std::ofstream file(filename, std::ios::trunc);
+    if (!file.is_open()) {
+        return false;
+    }
 
-   file << oss.str();
-   return file.good();
+    file << oss.str();
+    return file.good();
 }
