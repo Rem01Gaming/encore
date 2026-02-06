dd106255279b387711e3047241498376cfffa7bf
Rem01Gaming <Rem01_Gaming@proton.me>
2026-02-06 22:54:00 +0700

    webui: Add new icons
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

---
+++ webui/src/components/icons/DotsVertical.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg :width="size" :height="size" viewBox="0 0 24 24" :fill="color">
+    <path
+      d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z"
+    />
+  </svg>
+</template>
+
+<script setup>
+const props = defineProps({
+  size: {
+    type: [String, Number],
+    default: 24,
+  },
+  color: {
+    type: String,
+    default: 'currentColor',
+  },
+})
+</script>
