ad02de38cb6416c1b15a46059f4fe863abd51ba7
Rem01Gaming <Rem01_Gaming@proton.me>
2026-04-20 17:12:38 +0700

    webui: Define the standard property 'line-clamp' for compatibility
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- webui/src/views/Settings.vue
+++ webui/src/views/Settings.vue
@@ -337,6 +337,7 @@ const closeExportModal = () => {
 <style scoped>
 .line-clamp-2 {
   display: -webkit-box;
+  line-clamp: 2;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
